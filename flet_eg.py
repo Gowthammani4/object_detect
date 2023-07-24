f3adf4a8444f0dde16d0ef5c568480b439b5d998
Gowthammani4 <gowthammani7065@gmail.com>
2023-07-24 19:03:53 +0530

    commit

---
+++ flet_eg.py
@@ -0,0 +1,104 @@
+from typing import Dict
+from ultralytics import YOLO
+import flet
+from flet import *
+import cv2
+def image_predict(name):
+    file_upload=f"uploads/{name}"
+    classNames={0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 
+    8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 
+    14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 
+    22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase',
+     29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 
+     35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 
+     40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 
+     47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog',
+      53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 
+      59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 
+      65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven',
+       70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 
+    74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
+    img=cv2.imread(file_upload)
+    model=YOLO("yolov8n.pt",)
+    results=model(file_upload)
+    for i in results:
+        boxes=i.boxes
+        for box in boxes:
+            x1,y1,x2,y2=box.xyxy[0]
+            x1,y1,x2,y2=int(x1),int(y1),int(x2),int(y2)
+            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),4)
+            conf=(box.conf[0]*100)/100
+            class_=int(box.cls[0])
+            class_name=classNames[class_]
+            label=f'{class_name}{conf}'
+            t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+            c2 = x1 + t_size[0], y1 - t_size[1] - 3
+            cv2.rectangle(img, (x1,y1), c2, [255,0,255], -1, cv2.LINE_AA)  # filled
+            cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
+            cv2.imwrite("assets/predicted.jpeg",img)
+            print("image predicted")
+
+
+
+def main(page: Page):
+    prog_bars: Dict[str, ProgressRing] = {}
+    files = Ref[Column]()
+    upload_button = Ref[ElevatedButton]()
+
+    def file_picker_result(e: FilePickerResultEvent):
+        upload_button.current.disabled = True if e.files is None else False
+        prog_bars.clear()
+        files.current.controls.clear()
+        if e.files is not None:
+            for f in e.files:
+                prog = ProgressRing(value=0, bgcolor="#eeeeee", width=20, height=20)
+                prog_bars[f.name] = prog
+                files.current.controls.append(Row([prog, Text(f.name)]))
+        page.update()
+
+    def on_upload_progress(e: FilePickerUploadEvent):
+        prog_bars[e.file_name].value = e.progress
+        prog_bars[e.file_name].update()
+
+    file_picker = FilePicker(on_result=file_picker_result, on_upload=on_upload_progress)
+
+    def upload_files(e):
+        uf = []
+        if file_picker.result is not None and file_picker.result.files is not None:
+            for f in file_picker.result.files:
+                uf.append(
+                    FilePickerUploadFile(
+                        f.name,
+                        upload_url=page.get_upload_url(f.name, 600),
+                    )
+                )
+            file_picker.upload(uf)
+        image_predict(file_picker.result.files[0].name)
+        page.update()
+        page.add(Image(src="assets/predicted.jpeg",height=800,width=800))
+        print("image displaying")
+        page.update()
+
+    # hide dialog in a overlay
+    page.overlay.append(file_picker)
+
+
+    page.add(
+        ElevatedButton(
+            "Select files...",
+            icon=icons.FOLDER_OPEN,
+            on_click=lambda _: file_picker.pick_files(allow_multiple=True),
+        ),
+        Column(ref=files),
+        ElevatedButton(
+            "Upload",
+            ref=upload_button,
+            icon=icons.UPLOAD,
+            on_click=upload_files,
+            disabled=True,
+        ),
+    )
+    
+
+
+flet.app(target=main, upload_dir="uploads",assets_dir="assets", view=flet.WEB_BROWSER)
